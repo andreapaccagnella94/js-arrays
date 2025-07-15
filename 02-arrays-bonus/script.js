61be34d83f4012de5a117dfb01ddf12ed6401577
andreapaccagnella94 <andreapacca94@live.it>
2025-07-15 19:39:33 +0200

    add correction with comments for bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -20,6 +20,17 @@ console.log(reversedTeachers);
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = [];
+
+//👀OCCHIO BISOGNA SEMPRE DEFINIRE LA VARIABILE ALL'INIZIO CHE TI DA AD OGNI ITERAZIONE
+/* CE LO CONSIGLI ANCHE EMMET
+
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  
+}
+
+*/
+
 for (let index = 0; index < teachers.length; index++) {
   if (teachers[index].length >= 5) {
 
@@ -38,7 +49,23 @@ console.log(teachers);
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+/* SBAGLIATO
 const isFabioPresent = teachers.indexOf("Fabio");
+console.log(isFabioPresent);
+
+*/
+
+//CORRETTO VEDI VIDEO-LEZIONE
+let isFabioPresent = null;
+
+for (index = 0; index < teachers.length; index++) {
+  const teacher = teachers[index];
+  if (teacher === "Fabio") {
+    isFabioPresent = true;
+    break
+  }
+}
+
 console.log(isFabioPresent);
 
 
